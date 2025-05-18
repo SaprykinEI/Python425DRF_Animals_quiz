4314db871a88046c673d41c608dcd28936496ff9
SaprykinEI <saprykin.ei@yandex.ru>
2025-05-18 12:58:27 +0300

    1

--- users/user_serializers.py
+++ users/user_serializers.py
@@ -23,7 +23,7 @@ class UserCreateSerializer(serializers.ModelSerializer):
         ]
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        user = User.objects.create(**validated_data)
         user.set_password(user.password)
         user.save()
         return user
